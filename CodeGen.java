609d3f52b6c0d2f31807ddd4dcf88ddb53a00d02
Toghy1 <3657818686@qq.com>
2026-07-06 20:08:50 +0800

    Add files via upload

---
+++ CodeGen.java
@@ -0,0 +1,7 @@
+﻿package com.atguigu.tms.mock;
+
+/** Auto-decompiled from tms-mock JAR */
+public class com.atguigu.tms.mock.CodeGen {
+  public com.atguigu.tms.mock.CodeGen();
+  public static void main(java.lang.String[]) throws java.sql.SQLException;
+}
---
+++ TmsMockApplication.java
@@ -0,0 +1,9 @@
+﻿package com.atguigu.tms.mock;
+
+/** Auto-decompiled from tms-mock JAR */
+public class com.atguigu.tms.mock.TmsMockApplication {
+  private static final org.slf4j.Logger log;
+  public com.atguigu.tms.mock.TmsMockApplication();
+  public static void main(java.lang.String[]);
+  static {};
+}
